e399f8d32919e07d3c902628884c1127308a5883
Igor Souto <igor_souto@outlook.com>
2021-08-07 03:31:56 -0300

    feat(contact): add contact api route
    Add contact api route integrating with SendGrid.

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+
+interface EmailAttributes {
+  subject: string,
+  senderName: string,
+  senderEmail: string,
+  text: string,
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') return res.status(404).send('not_found');
+
+  const emailTemplate: EmailAttributes = req.body;
+
+  const email = {
+    to: 'igor_souto@outlook.com',
+    from: 'institutopedrohenriquedh@gmail.com',
+    templateId: 'd-7d86c013ca18464d8eb10c53783d0393',
+    ip_pool: 'transactional',
+    dynamic_template_data: emailTemplate,
+  };
+
+  sgMail.send(email);
+
+  return res.status(200).json({});
+}
